84344312cab0ad36c6fc7118b6aff4fab9313203
Matt Spinelli <651748+mspinelli@users.noreply.github.com>
2025-03-20 17:43:13 -0400

    fix comment typos and add some details

--- src/smolagents/smolagent.py
+++ src/smolagents/smolagent.py
@@ -5,8 +5,8 @@ from phoenix.otel import register
 
 from smolagents import CodeAgent, DuckDuckGoSearchTool, LiteLLMModel
 
-# Setup telemetry traces for the agent. The code in this modules will run
-# inside of docker and assumes the telemtry server (phoenix) is running on
+# Setup telemetry traces for the agent. The code in this module will run
+# inside of docker and assumes the telemetry server (phoenix) is running on
 # the host machine.
 #
 # See https://huggingface.co/docs/smolagents/tutorials/inspect_runs for
@@ -22,10 +22,12 @@ SmolagentsInstrumentor().instrument()
 # (e.g. HfApiModel), the code here uses Ollama through LiteLLMModel. Ollama is
 # a which is a local server that can run any GGUF model downloaded from
 # Huggingface. In this case a quantized model for Qwen2.5-Coder-32B-Instruct
-# has been downloaded on the host machine already. As with the telemtry server,
+# has been downloaded on the host machine already. As with the telemetry server,
 # this code assumes that we are running Ollama on the host machine as well.
 # On macos, Ollama cannot run inside of docker and utilize the GPU. This is why
-# it must be installed on the host machine.
+# it must be installed on the host machine. Linux host machines, I believe, can
+# run Ollama inside of docker and utilize the GPU. But this workaround should
+# work on either Linux or Mac.
 model = LiteLLMModel(
     model_id=f"ollama_chat/{os.getenv("OLLAMA_MODEL_ID")}",
     api_base=f"http://host.docker.internal:{int(os.getenv("OLLAMA_PORT"))}",
@@ -37,8 +39,8 @@ response = agent.run(
     "Search for the best music recommendations for a party at the Wayne's mansion."
 )
 
-# If we weren't using telementry then this becomes more important, but the
+# If we weren't using telemetry then this becomes more important, but the
 # output of the response will show in a more structured way in on the host
-# machine via the telemtry daashboard at http://127.0.0.1:6006/projects by
+# machine via the telemetry dashboard at http://127.0.0.1:6006/projects by
 # default
 print(response)
